dbe1df23aa46537bab5a8ca84c301b5aeef92b7e
DASH <shivansh.tyagi@vidyamantra.com>
2022-10-07 10:56:55 +0530

    Applied v-for on Button Render

--- app.vue
+++ app.vue
@@ -1,10 +1,32 @@
 <script setup lang="ts">
-
+const buttonList = {
+    gender: ['Boy', 'Unisex', 'Girl'],
+    popularity: ['Trendy', 'Unique'],
+    length: ['Long', 'All', 'Short']
+};
 const option = ref({
     gender: 'Unisex',
     popularity: 'Trendy',
-    lenght: 'Short'
-})
+    length: 'Short'
+});
+
+const checkClass = (elem: string[], key: string, btn: string, index: number) => {
+    let str = '';
+    if (index === 0)
+        str += ' rounded-l-full '
+    if (index === elem.length - 1)
+        str += ' rounded-r-full '
+    if (btn === option.value[key])
+        str += ' active '
+    return str;
+}
+
+const btnClick = (e: MouseEvent, key: string) => {
+    const target = e.target as HTMLElement;
+    option.value[key] = target.innerText;
+    return e;
+}
+
 </script>
 
 <template>
@@ -12,36 +34,23 @@ const option = ref({
     <div class="text-center p-20">
         <h1 class="text-red-400 text-8xl my-10">Name Generator</h1>
         <div class="bg-red-100 rounded-[3rem] w-[60rem] mx-auto py-20 ">
-            <div class="option-container my-8">
-                <button :class="option.gender === 'Boy' && 'bg-red-400 active'" class="btn rounded-l-full">Boy</button>
-                <button :class="option.gender === 'Unisex' && 'bg-red-400 active'" class="btn">Unisex</button>
-                <button :class="option.gender === 'Girl' && 'bg-red-400 active'"
-                    class='btn rounded-r-full'>Girl</button>
-            </div>
-            <div class="option-container my-8">
-                <button :class="option.popularity === 'Trendy' && 'bg-red-400 active'"
-                    class='btn rounded-l-full'>Trendy</button>
-                <button :class="option.popularity === 'Unique' && 'bg-red-400 active'"
-                    class='btn rounded-r-full'>Unique</button>
-                <div class="option-container my-8">
-                    <button :class="option.lenght === 'Long' && 'bg-red-400 active'"
-                        class='btn rounded-l-full'>Long</button>
-                    <button :class="option.lenght === 'All' && 'bg-red-400 active'" class='btn'>All</button>
-                    <button :class="option.lenght === 'Short' && 'bg-red-400 active'"
-                        class='btn rounded-r-full'>Short</button>
-                </div>
+            <div v-for="(elem, key) in buttonList" class="option-container my-8">
+                <button v-for="(btn, index) in elem" class="btn" :class="checkClass(elem, key, btn, index)"
+                    @click="(e: MouseEvent) => btnClick(e, key)">
+                    {{btn}}
+                </button>
             </div>
         </div>
     </div>
 
 </template>
 
-<style lang="postcss" scoper>
+<style lang="postcss" scope>
 .btn {
     @apply text-2xl w-32 h-14 border-2 border-red-400 text-indigo-400
 }
 
 .active {
-    @apply text-white
+    @apply text-white bg-red-400
 }
 </style>
